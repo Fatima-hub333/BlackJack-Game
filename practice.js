8a023605a8bab581abdf67c3fb5b5a990b29236a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-20 11:41:19 +0500

    loop practice

--- practice.js
+++ practice.js
@@ -108,3 +108,41 @@ console.log(messages);
 
 messages.pop();
 console.log(messages);
+
+//Counting JavaScript
+for (let count = 1; count < 11; count += 2 ) {
+  console.log(count);
+}
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+for (let count = 10; count < 101; count += 10) {
+  console.log(count);
+}
+
+//For loops and arrays
+let messages = [
+  "Hey, how's it going?",
+  "I'm great, thank you! How about you?",
+  "All good. Been working on my protfolio lately.",
+  "Same here!"
+]
+
+//DRY - Don't Repeat Yourself
+// console.log(messages[0]);
+// console.log(messages[1]);
+// console.log(messages[2]);
+// console.log(messages[3]);
+
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
+
+let chairs = [7, 3, 9];
+
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
+
+for (let i = 0; i < chairs.length; i += 1) {
+  console.log(chairs[i]);
+}
