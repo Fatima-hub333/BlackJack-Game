48d24285b25cd8a3256380cc17a069b82a820bea
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-20 11:07:03 +0500

    Create game UI, functions

--- index.js
+++ index.js
@@ -1,75 +1,45 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
-// let firstCard = 10;
-// let secondCard = 7;
-// 2. Create a variable, sum, and set it to the sum of the two cards
-// let sum = firstCard + secondCard;
-
-// if (sum < 21) {
-//   console.log("Do you want to draw a new card? 😀");
-// } else if (sum === 21) {
-//   console.log("Wohoo! You've got BlackJack! 🙃");
-// } else{
-//   console.log("You're out of the game!");
-// }
-// console.log(sum);
-
-
-// Check if the person is old enough to enter the nightclub (21)
-// Log a suitable message to the console in both cases
-
-// let age = 22
-
-// if (age < 21) {
-//   console.log("You can not enter the club!")
-// } else {
-//   console.log("Welcome")
-// }
-
-// if less than 21 -> "You can not enter the club!"
-// else            -> "Welcome!"
-
-// Check if the person is elegible for a birthday card from the King! (100)
-
-// let age = 150
-
-// if (age < 100) {
-//   console.log("Not elegible")
-// } else if (age === 100) {
-//   console.log("Here is your birthday card from the King!")
-// } else {
-//   console.log("Not elegible, you have already gotten one")
-// }
-
-// if less than 100    -> "Not elegible"
-// else if exactly 100 -> "Here is your birthday card from the King!"
-// else                -> "Not elegible, you have already gotten one"
-
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
+let firstCard = 10;
+let secondCard = 11;
+let cards = [firstCard, secondCard] //array - ordered list of items
+let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack! 🥳" ;
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game! 😭";
-  isAlive = false;
+let messageEl = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.getElementById('cards-el');
+// let sumEl = document.querySelector('#sum-el');
+
+function startGame() {
+  renderGame();
 }
 
-console.log(message);
+function renderGame() {
+  sumEl.textContent = "Sum: " + sum;
+  // render out firstCard and secondCard
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  // render out All the cards we have
+
+  if (sum <= 20) {
+  message = "Do you want to draw a new card?";
+} else if (sum === 21) {
+  message = "Wohoo! You've got Blackjack!" ;
+  hasBlackJack = true;
+} else {
+  message = "You're out of the game!";
+  isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = 7;
+  sum += card;
+  cards.push(card);
+  renderGame();
+}
 
 
-// console.log(4 === 3)  // False
-// console.log(5 > 2)    // true 
-// console.log(12 > 12)  // false
-// console.log(3 < 0)    // false
-// console.log(3 >= 3)   // true
-// console.log(11 <= 11) // true
-// console.log(3 <= 2)   // false
+
 
