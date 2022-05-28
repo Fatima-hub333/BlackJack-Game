c3f7c5c904622b722e01494803c491bc334fab74
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-28 21:39:46 +0500

    Fix issues in functionality

--- index.js
+++ index.js
@@ -1,95 +1,68 @@
 let player = {
-  name: "Fatima", chips: 200,
-  sayHello: function () {
-  console.log("Heissan!")
-  }
+    name: "Per",
+    chips: 200
 }
 
-player.sayHello()
-let cards = [] //array - ordered list of items
-let sum = 0;
-let hasBlackJack = false;
-let isAlive = false;
-let message = "";
-let messageEl = document.getElementById('message-el');
-let sumEl = document.getElementById('sum-el');
-let cardsEl = document.getElementById('cards-el');
-// let sumEl = document.querySelector('#sum-el');
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
-let playerEl = document.getElementById('player-el');
-playerEl.textContent = player.name + ": $" + player.chips;
+playerEl.textContent = player.name + ": $" + player.chips
 
-// Create a function, getRandomCard(), that always returns the number 5
 function getRandomCard() {
-  let randomNumber = Math.floor(Math.random() * 13) + 1;
-  if (randomNumber > 10) {
-    return 10;
-  } else if (randomNumber === 1) {
-    return 11;
-  } else {
-    return randomNumber;
-  }
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
 function startGame() {
-  isAlive = true;
-  let firstCard = getRandomCard();
-  let secondCard = getRandomCard();
-  cards = [firstCard, secondCard];
-  sum = firstCard + secondCard;
-  renderGame();
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: ";
-  //Create a for loop that renders out all the cards instead of just two
-  for (let i = 0; i < cards.length; i++) {
-    cardsEl.textContent += cards[i] + " ";
-  }
-
-  sumEl.textContent = "Sum: " + sum;
-
-  if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack!" ;
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game!";
-  isAlive = false;
-  }
-  messageEl.textContent = message;
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
+
 
 function newCard() {
-  //Only allow the player to get a new card if she is alie and doesnot have blackJack
-  if (isAlive === true && hasBlackJack === false) {
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  renderGame();
-  }
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
-
-// Objects Practice
-// Objects - store data in-depth - composite / complex data type
-// key-value pairs
-
-// let course = {
-//   title: "Learn CSS GRid for Free",
-//   lessons: 16,
-//   creator: "Fatima",
-//   length: 63,
-//   level: 2,
-//   isFree: true,
-//   tags: ["html", "css"]
-// }
-
-// console.log(course.tags)
-
-
-
-
 
 
 
--- practice.js
+++ practice.js
@@ -244,3 +244,137 @@ let castle = {
 
 console.log(castle.price)
 console.log(castle.isSuperHost)
+
+// Objects Practice
+// Objects - store data in-depth - composite / complex data type
+// key-value pairs
+
+let course = {
+  title: "Learn CSS GRid for Free",
+  lessons: 16,
+  creator: "Fatima",
+  length: 63,
+  level: 2,
+  isFree: true,
+  tags: ["html", "css"]
+}
+
+console.log(course.tags)
+
+// Create a person object that contains three keys: name, age, and county.
+// Use yourself as an example to set the values for name, age, and country
+let person = {
+  name: "Fatima",
+  age: 25,
+  country: "Pakistan"
+}
+
+// Create a function, logData(), that uses the person object to create a
+// string in the following format: 
+// "Per is 35 years old and lives in Norway"
+function logData() {
+  console.log(person.name + " is " + person.age + " years old and lives in " + person.country);
+}
+// Call the logData() function to verify that it works
+logData();
+
+let age = 55
+
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+if (age < 6) {
+  console.log("Free");
+} else if (age < 18) {
+  console.log("Child Discount");
+} else if (age < 27) {
+  console.log("Student Discount");
+} else if (age < 67) {
+  console.log("Full Price");
+} else {
+  console.log("Senior Citizen Discount");
+}
+
+console.log("age");
+
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
+console.log("The 5 larget countries in the world: ")
+for (let i = 0; i < largeCountries.length; i++) {
+  console.log("- " + largeCountries[i])
+}
+
+let movieNames = ["Damak", "Dam Mastum", "Dr.Strange", "Avengers", "Dil Lagy"]
+
+console.log("The 5 important movies of Pakistan are: ")
+for (let i = 0; i < movieNames.length; i++) {
+  console.log("- " + movieNames[i]);
+}
+
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+largeCountries.pop();
+largeCountries.push("Pakistan");
+largeCountries.shift();
+largeCountries.unshift("China");
+
+
+
+console.log(largeCountries);
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+
+let dayOfMonth = 13
+let weekday = "Friday"
+
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+if (dayOfMonth === 13 && weekday === "Friday") {
+  console.log("😃")
+}
+
+let hands = ["rock", "paper", "scissor"];
+// Create a function that returns a random item from the array
+function getItems() {
+  let randomIndex = Math.floor(Math.random()*3);
+  return hands[randomIndex];
+}
+
+console.log(getItems());
+
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+
+function sortFruit() {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+            appleShelf.textContent += "🍎"
+        } else if (fruit[i] === "🍊") {
+            orangeShelf.textContent += "🍊"
+        }
+    }
+}
+
+sortFruit()
